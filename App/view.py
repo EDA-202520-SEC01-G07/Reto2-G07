dc4900b5c83660b29f51b225f069b56ccdbd241b
Juliana-_Rod-1234 <js.rodriguezm1234@uniandes.edu.co>
2025-10-03 21:03:32 -0500

    Correcciones

--- App/view.py
+++ App/view.py
@@ -35,7 +35,7 @@ def load_data(control):
     print("\nTiempo de carga: "+str(tiempo)+" [ms].\
         \nTotal de trayectos: "+str(total)+" trayectos.\
         \nEl trayecto con menor distancia es el "+str(menorid)+":  \t Distancia: "+str(menor)+" [millas]\t Fecha: "+str(fecha_menor)+"\tCosto: $"+str(costo_menor)+"\n\
-        \nEl trayecto con mayor distancia es el "+str(mayorid)+":  \t Distancia: "+str(mayor)+" [millas]\t Fecha: "+str(fecha_mayor)+"\tCosto: $s"+str(costo_mayor)+"\n\
+        \nEl trayecto con mayor distancia es el "+str(mayorid)+":  \t Distancia: "+str(mayor)+" [millas]\t Fecha: "+str(fecha_mayor)+"\tCosto: $"+str(costo_mayor)+"\n\
         \nLos primeros 5 viajes fueron:\n"+ tb.tabulate(primeros, headers="keys") +"\n\nLos últimos 5 viajes fueron: \n"+ tb.tabulate(ultimos, headers="keys")+"\n")
 
 
@@ -53,118 +53,33 @@ def print_req_1(control):
         Función que imprime la solución del Requerimiento 1 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 1
-    pas = input("Indique la cantidad de pasajeros: ").strip()
-    tiempo, trayectos, duracion_prom, costo_prom, distancia_prom, peajes_prom, pago_mas_usado, propina_prom, fecha_repetida, frecuencia = logic.req_1(control, pas)
-    print("\nTiempo de ejecución: "+str(tiempo)+" [ms].\
-        \nCantidad de trayectos con "+str(pas)+" pasajeros: "+str(trayectos)+"\
-        \nEstadísitcas:\n\t- Duración promedio [min] de los trayectos: "+str(duracion_prom)+"\
-        \n\t- Costo promedio [dólares] de los trayectos: "+str(costo_prom)+"\
-        \n\t- Distancia promedio [millas] de los trayectos: "+str(distancia_prom)+"\
-        \n\t- Promedio costos de peajes: "+str(peajes_prom)+"\
-        \n\t- Tipo de pago más usado: "+str(pago_mas_usado)+"\
-        \n\t- Pago propina promedio de los trayectos: "+str(propina_prom)+"\
-        \n\t- La fecha con más trayectos es "+fecha_repetida+" donde hubo "+str(frecuencia)+" trayectos.")
+    
 
 def print_req_2(control):
     """
         Función que imprime la solución del Requerimiento 2 en consola
     """
-    pago = input("Indique el tipo de pago que desea consultar (CREDIT_CARD, CASH, NO_CHARGE, UNKNOWN): ").strip().upper()
-    tiempo, contador, duracion, costo, distancia_total, peajes_total, cantidad_pasa, propina, fecha_mayor = logic.req_2(control, pago)
-
-    data = [
-        ["Tiempo de consulta [ms]", str(tiempo)],
-        ["Total de trayectos con tipo de pago " + str(pago), str(contador)],
-        ["Duración promedio de los viajes [min]", str(duracion)],
-        ["Costo promedio de los viajes [USD]", str(costo)],
-        ["Distancia promedio de los viajes [millas]", str(distancia_total)],
-        ["Peajes promedio de los viajes [USD]", str(peajes_total)],
-        ["Cantidad de pasajeros más frecuente “#número de pasajeros – cantidad”", str(cantidad_pasa)],
-        ["Propina promedio [USD]", str(propina)],
-        ["Fecha de finalización de trayecto con mayor frecuencia ", str(fecha_mayor)]
-    ]
-
-    print("\n=== Resultados del Requerimiento 2 ===")
-    print(tb.tabulate(data, headers=["Descripción", "Valor"]))
-    print("=======================================\n")
-
+    # TODO: Imprimir el resultado del requerimiento 2
+    
 def print_req_3(control):
     """
         Función que imprime la solución del Requerimiento 3 en consola
     """
-    menor= input("Indique el valor menor del precio total pagado: ").strip()
-    mayor= input("Indique el valor mayor del precio total pagado: ").strip()
-    tiempo, contador, duracion, costo, distancia_total, peajes_total, cantidad_pasa, propina, fecha_mayor = logic.req_3(control, mayor, menor)  
-    print("\nTiempo de ejecución: "+str(tiempo)+" [ms].\
-        \nCantidad de trayectos con precio total pagado entre "+str(menor)+" y "+str(mayor)+": "+str(contador)+"\
-        \nEstadísitcas:\n\t- Duración promedio [min] de los trayectos: "+str(duracion)+"\
-        \n\t- Costo promedio [dólares] de los trayectos: "+str(costo)+"\
-        \n\t- Distancia promedio [millas] de los trayectos: "+str(distancia_total)+"\
-        \n\t- Promedio costos de peajes: "+str(peajes_total)+"\
-        \n\t- Cantidad de pasajeros más frecuente “#número de pasajeros – cantidad”: "+str(cantidad_pasa)+"\
-        \n\t- Pago propina promedio de los trayectos: "+str(propina)+"\
-        \n\t- La fecha con más trayectos es "+fecha_mayor+".")
-    return None
-
+    # TODO: Imprimir el resultado del requerimiento 3
+    
 
 def print_req_4(control):
     """
         Función que imprime la solución del Requerimiento 4 en consola
     """
-    filtro = input("Digite filtro (MAYOR/MENOR): ").strip().upper()
-    fecha_ini = input("Digite fecha inicial (YYYY-MM-DD): ").strip()
-    fecha_fin = input("Digite fecha final (YYYY-MM-DD): ").strip()
-
-    tiempo, filtro, total, origen, destino, dist_prom, dur_prom, costo_prom = logic.req4(
-        control, filtro, fecha_ini, fecha_fin
-    )
-
-    if total == 0:
-        print("\nNo se encontraron trayectos en el rango de fechas.")
-    else:
-        data = [
-            ["Tiempo de consulta [ms]", str(round(tiempo, 2))],
-            ["Filtro aplicado", str(filtro)],
-            ["Total de trayectos en el rango de fechas", str(total)],
-            ["Barrio de origen", str(origen)],
-            ["Barrio de destino", str(destino)],
-            ["Distancia promedio de los trayectos [millas]", str(round(dist_prom, 2))],
-            ["Duración promedio de los trayectos [min]", str(round(dur_prom, 2))],
-            ["Costo promedio de los trayectos [USD]", str(round(costo_prom, 2))]
-        ]
-
-        print("\n=== Resultados del Requerimiento 4 ===")
-        print(tb.tabulate(data, headers=["Descripción", "Valor"]))
-        print("=======================================\n")
-
-
+    # TODO: Imprimir el resultado del requerimiento 4
+    
+    
 def print_req_5(control):
     """
         Función que imprime la solución del Requerimiento 5 en consola
     """
-    filtro = input("Digite filtro (MAYOR/MENOR): ").strip().upper()
-    fecha_ini = input("Digite fecha inicial (YYYY-MM-DD): ").strip()
-    fecha_fin = input("Digite fecha final (YYYY-MM-DD): ").strip()
-    
-    tiempo_ms, filtro_usado, total_trayectos_en_rango, franja, costo_promedio, num_trayectos_franja, duracion_promedio_min, pasajeros_promedio, costo_max_franja, costo_min_franja = logic.req_5(control, filtro, fecha_ini, fecha_fin)
-    
-    if franja is None:
-        print("\nNo hay franjas con datos en ese rango de fechas.")
-        return None
-    
-    print("\nTiempo de ejecución: " + str(tiempo_ms) + " [ms]." +
-        "\nFiltro aplicado: " + str(filtro_usado) +
-        "\nCantidad de trayectos en el rango de fechas: " + str(total_trayectos_en_rango))
-
-    print("\nFranja horaria seleccionada: " + str(franja) +
-        "\n\t- Costo promedio [USD]: " + str(costo_promedio) +
-        "\n\t- Número de trayectos: " + str(num_trayectos_franja) +
-        "\n\t- Duración promedio [min]: " + str(duracion_promedio_min) +
-        "\n\t- Pasajeros promedio: " + str(pasajeros_promedio) +
-        "\n\t- Costo total máximo en la franja [USD]: " + str(costo_max_franja) +
-        "\n\t- Costo total mínimo en la franja [USD]: " + str(costo_min_franja))
-    return None
-
+    # TODO: Imprimir el resultado del requerimiento 5
 
 
 def print_req_6(control):
@@ -172,18 +87,7 @@ def print_req_6(control):
         Función que imprime la solución del Requerimiento 6 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 6
-    barrio = input("Indique qué barrio quiere evaluar: ").strip()
-    fecha_i = input("Digite fecha inicial (YYYY-MM-DD): ").strip()
-    fecha_f = input("Digite fecha final (YYYY-MM-DD): ").strip()
-    t, trayectos, distancia_prom, tiempo_prom, destino_repetido, info_pagos = logic.req_6(control, barrio, fecha_i,fecha_f)
-    print(
-        "\nTiempo de ejecución: "+str(t)+" [ms].\
-        \nCantidad de trayectos hechos en el rango de fechas y desde el barrio dado: "+str(trayectos)+"\
-        \nDistancia promedio [millas] de los trayectos: "+str(distancia_prom)+"\
-        \nTiempo promedio [min] de los trayectos: "+str(tiempo_prom)+"\
-        \n\nBarrio más visitado como destino de los trayectos: "+str(destino_repetido)+" \
-        \n\nDatos para cada medio de pago:\n"+tb.tabulate(info_pagos, headers="keys",tablefmt="simple")+"\n\n")
-
+    
 
 # Se crea la lógica asociado a la vista
 control = new_logic()
