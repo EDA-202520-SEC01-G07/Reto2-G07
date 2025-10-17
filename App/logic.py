d97445ba34a479c3ebaa05134d661daec1f340be
jlozadab <j.lozadab@uniandes.edu.co>
2025-10-16 21:18:50 -0500

    req 3 hecho*

--- App/logic.py
+++ App/logic.py
@@ -187,6 +187,7 @@ def req_3(catalog, d_ini, d_fin, n):
             trayectos += 1
             lt.add_last(filtrado, viaje)
 
+
     filtrado = lt.quick_sort(filtrado, sort_crit_dist)
 
     end = get_time()
